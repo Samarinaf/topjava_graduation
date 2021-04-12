b7c52e94fba428734368a7860e3ae604178d92d4
Yana Samarina <yanasamarina1@gmail.com>
2021-04-12 22:09:21 +0300

    Add vote

---
+++ src/main/java/com/topjava/graduation/model/Vote.java
@@ -0,0 +1,70 @@
+package com.topjava.graduation.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "votes")
+public class Vote extends AbstractBaseEntity {
+
+    @Column(name = "date")
+    @NotNull
+    private LocalDate date;
+
+    @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
+    private User user;
+
+    @Column(name = "restaurant_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    @NotNull
+    private Restaurant restaurant;
+
+    public Vote() {
+    }
+
+    public Vote(Integer id, LocalDate date, User user, Restaurant restaurant) {
+        super(id);
+        this.date = date;
+        this.user = user;
+        this.restaurant = restaurant;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                ", date=" + date +
+                ", user=" + user +
+                ", restaurant=" + restaurant +
+                '}';
+    }
+}
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -2,6 +2,8 @@ DELETE FROM user_roles;
 DELETE FROM users;
 DELETE FROM restaurants;
 DELETE FROM dishes;
+DELETE FROM votes;
+
 ALTER SEQUENCE GLOBAL_SEQ RESTART WITH 1000;
 
 INSERT INTO users (name, email, password)
@@ -21,3 +23,8 @@ VALUES ('BlueRestaurant'), --1002
 INSERT INTO dishes (name, price, restaurant_id)
 VALUES ('Chicken', 780, 1002),
        ('Coffee', 250, 1002);
+
+INSERT INTO votes (date, user_id, restaurant_id)
+VALUES ('2021-04-12', 1001, 1002),
+       ('2021-04-11', 1001, 1005),
+       ('2021-04-11', 1000, 1003);
